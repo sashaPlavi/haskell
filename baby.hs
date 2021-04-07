148923462b294988295cf69effce36907c450ab4
SashaPlavi <sasha.bingulac@gmail.com>
2021-04-07 11:11:26 +0200

    baby functions

---
+++ baby.hs
@@ -0,0 +1,12 @@
+ import Data.List
+ import System.IO
+
+--function 1
+doubleMe x = x + x
+
+--function2
+doubleSmallNumber x = if x > 100  
+                        then x  
+                        else x*2 
+
+  
